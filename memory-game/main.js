0e94c2590f57d630c332fdfddf359311bd51de6a
hayaAbdulaziz <46543455+hayaAbdulaziz@users.noreply.github.com>
2020-01-19 11:14:40 +0300

    created game logic

---
+++ memory-game/main.js
@@ -0,0 +1,8 @@
+
+console.log("Up and running!");
+let cardOne = "queen";
+let cardTow = "king";
+let cardThree = "queen";
+let cardFour= "King";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTow);
